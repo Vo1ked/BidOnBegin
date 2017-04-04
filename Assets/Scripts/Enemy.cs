f0a2d3d56f8e0fbca95641cf4c30fa235c6bd3d7
Vo1ked <Volk.xxx@mail.ru>
2017-04-04 10:49:02 +0300

    Enemy Canvac lookt at player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,18 +5,26 @@ using UnityEngine.UI;
 public class Enemy : MonoBehaviour
 {
     public Text TextArea;
+
     public Slider SlideArea;
+
+    [SerializeField] public Canvas Canvas;
+
+    private GameObject _player;
+    private GameObject _camera;
 
 	// Use this for initialization
 	void Start () {
-	
+	    _player = GameObject.FindGameObjectWithTag("Player");
+	    _camera = GameObject.FindWithTag("MainCamera");
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        Canvas.transform.LookAt(_camera.transform.position);
 	    if (SlideArea.value <1)
 	    {
-	        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>().EnemyTarget = null;
+	        _player.GetComponent<PlayerControl>().EnemyTarget = null;
             Destroy(gameObject);
 
         }
